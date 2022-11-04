235905cb65cd4ae4ca29179934814bf9141818f9
jshprintz <jshprintz@gmail.com>
2022-11-03 20:33:29 -0700

    added login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,78 @@
 import React, { useState } from "react";
-import "./LoginPage.css";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import userService from "../../utils/userService";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function LoginPage(props) {
+  const [error, setError] = useState("");
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await userService.login(state);
+      props.handleSignUpOrLogin();
+      navigate("/");
+    } catch (err) {
+      console.log(err, "<--ERROR IN LOGIN PAGE");
+      setError(err.message);
+    }
+  }
+
   return (
     <>
-      <h1>Setup Login Page</h1>
-      <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
-        <li>
-          Make sure you read the Login Controller, to know how it is setup to
-          find the user!
-        </li>
-      </ul>
+      <div className="centered">
+        <br />
+        <a href="/" className="large-font-size centered white-back login-title">
+          ART HOME
+        </a>
+      </div>
+      <br />
+      <br />
+
+      <p color="grey" textAlign="center">
+        Log in to your account
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={state.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          value={state.password}
+          onChange={handleChange}
+          required
+        />
+        <button color="grey" fluid size="large" type="submit" className="btn">
+          Login
+        </button>
+      </form>
+
+      <Link to="/signup">
+        New to us? <Link to="/signup">Sign Up</Link>
+      </Link>
+      {error ? <ErrorMessage error={error} /> : null}
     </>
   );
 }
