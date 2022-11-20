ebc6052b3b95fc33b0cbca09681d5d34feb73899
jshprintz <jshprintz@gmail.com>
2022-11-20 12:55:28 -0800

    avatar display on correct side. Need to have it overlap instead of pushing down.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,48 +5,62 @@ import Image from "react-bootstrap/Image";
 import { CgProfile } from "react-icons/cg";
 import "./NavBar.css";
 
-
-
-function NavBar({ loggedUser, handleLogout, dispSubs, handleAvatarClick, avatarDisp }) {
-  console.log(avatarDisp, "<--")
+function NavBar({
+  loggedUser,
+  handleLogout,
+  dispSubs,
+  handleAvatarClick,
+  avatarDisp,
+}) {
+  console.log(avatarDisp, "<--");
   return (
-    <Navbar fixed="top" id="nav-bar" expand="md">
-      <Container>
-        <Navbar.Brand href="/">
-          <Image
-            id="logo-nav"
-            src="https://i.imgur.com/G4eja1w.png"
-            alt="logo"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          { dispSubs ? (
-          <Nav className="me-auto">
-            <Nav.Link href="#logo-details">Specialities</Nav.Link>
-            <Nav.Link href="#design-details-buffer">Styles</Nav.Link>
-            <Nav.Link href="#story-containor-buffer">Story</Nav.Link>
-            <Nav.Link href="#reviews-containor">Testimonials</Nav.Link>
+    <>
+      <Navbar fixed="top" id="nav-bar" expand="md">
+        <Container>
+          <Navbar.Brand href="/">
+            <Image
+              id="logo-nav"
+              src="https://i.imgur.com/G4eja1w.png"
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            {dispSubs ? (
+              <Nav className="me-auto">
+                <Nav.Link href="#logo-details">Specialities</Nav.Link>
+                <Nav.Link href="#design-details-buffer">Styles</Nav.Link>
+                <Nav.Link href="#story-containor-buffer">Story</Nav.Link>
+                <Nav.Link href="#reviews-containor">Testimonials</Nav.Link>
+              </Nav>
+            ) : null}
+          </Navbar.Collapse>
+          <Nav className="justify-content-end">
+            {loggedUser ? (
+              <Nav.Link onClick={handleAvatarClick}>
+                <Image
+                  src={loggedUser?.photoUrl}
+                  alt="profile pic"
+                  className="profile-pic"
+                  roundedCircle
+                />
+              </Nav.Link>
+            ) : (
+              <Nav.Link href="/login">
+                <CgProfile size={36} className="icons" />
+              </Nav.Link>
+            )}
           </Nav>
-          ) : null }
-        </Navbar.Collapse>
-        <Nav className="justify-content-end">
-            {loggedUser ?  (
-            <Nav.Link onClick={handleAvatarClick}>
-                <Image src={loggedUser?.photoUrl} alt="profile pic" className="profile-pic" roundedCircle />
-            </Nav.Link>
-            ) :
-            <Nav.Link href="/login">
-              <CgProfile size={36} className="icons" />
-            </Nav.Link>}
-          </Nav>
-      </Container>
+        </Container>
+      </Navbar>
       {avatarDisp ? (
-                <div id="avatar-menu">
-                  <p>It works!</p>
-                </div>
-                ) : null}
-    </Navbar>
+        <div id="avatar-menu-container">
+        <div id="avatar-menu">
+          <p>It works!</p>
+        </div>
+        </div>
+      ) : null}
+    </>
   );
 }
 
